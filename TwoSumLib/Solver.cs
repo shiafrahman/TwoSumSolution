c8e5dc21bced4eff0a65f0c145288bd0163e8f6c
Your Nameshiafrahman <shiafrahman@gmail.com>
2025-05-16 21:26:58 +0600

    Refactored TwoSum and added tests

---
+++ TwoSumLib/Solver.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoSumLib
+{
+    public class Solver
+    {
+        public int[] TwoSumBruteForce(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length; i++)
+                for (int j = i + 1; j < nums.Length; j++)
+                    if (nums[i] + nums[j] == target)
+                        return new[] { i, j };
+            return Array.Empty<int>();
+        }
+
+        public int[] TwoSum(int[] nums, int target)
+        {
+            Dictionary<int, int> map = new();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int complement = target - nums[i];
+                if (map.TryGetValue(complement, out int index))
+                    return new[] { index, i };
+
+                if (!map.ContainsKey(nums[i]))
+                    map[nums[i]] = i;
+            }
+            return Array.Empty<int>();
+        }
+    }
+}
---
+++ TwoSumTests/SolverTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwoSumLib;
+
+namespace TwoSumTests
+{
+    public class SolverTests
+    {
+        private readonly Solver _solver = new();
+
+        [Fact]
+        public void TwoSum_NormalCase_ReturnsIndices()
+        {
+            var result = _solver.TwoSum(new[] { 2, 7, 11, 15 }, 9);
+            Assert.Equal(new[] { 0, 1 }, result);
+        }
+
+        [Fact]
+        public void TwoSum_EdgeCase_EmptyArray_ReturnsEmpty()
+        {
+            var result = _solver.TwoSum(Array.Empty<int>(), 10);
+            Assert.Empty(result);
+        }
+
+        [Fact]
+        public void TwoSum_Duplicates_ReturnsCorrectIndices()
+        {
+            var result = _solver.TwoSum(new[] { 3, 3, 4 }, 6);
+            Assert.Equal(new[] { 0, 1 }, result);
+        }
+
+        [Fact]
+        public void TwoSum_NoSolution_ReturnsEmpty()
+        {
+            var result = _solver.TwoSum(new[] { 1, 2, 3 }, 7);
+            Assert.Empty(result);
+        }
+    }
+}
